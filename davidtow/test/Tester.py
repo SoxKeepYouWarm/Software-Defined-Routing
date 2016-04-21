799cc69d6f4225ba524ed7115dedf8f7f3686381
root <david115632042@gmail.com>
2016-04-21 00:58:19 -0400

    adding python testing script

--- davidtow/test/Tester.py
+++ davidtow/test/Tester.py
@@ -1,4 +1,5 @@
 import subprocess
+import argparse
 import time
 
 CONTROLLER = "../../controller/controller"
@@ -13,6 +14,7 @@ CONTROLLER_ERROR = "./output/controller_error.txt"
 
 OUTPUT_RULES = " 1>> " + CONTROLLER_OUTPUT + " 2>> " + CONTROLLER_ERROR
 
+IP_STEM = "172.20.0."
 IP_ADDRESS = "172.20.0.2"
 
 ROUTER_ID = " 1 "
@@ -29,29 +31,51 @@ def build_topology_file(num_of_routers):
     for num in range(0, num_of_routers):
         print num
         topology_file.write(str(num + 1) + " ")
-        topology_file.write(IP_ADDRESS + " ")
+        topology_file.write(IP_STEM + str(2 + num) + " ")
         topology_file.write(str(40000 + num) + " ")
         topology_file.write(str(1000) + " ")
         topology_file.write(str(2000))
         topology_file.write("\n")
 
-    for num in range(0, num_of_routers - 1):
+    for num in range(1, num_of_routers):
         topology_file.write(str(num) + " " + str(num + 1) + " 10\n")
+
+def send_cmd(CMD, ROUTER_ID="1", UPDATE_INTERVAL="3"):
+
 
 if __name__ == '__main__':
     print "running"
 
-    build_topology_file(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("router_count", type=int, help="Enter the number of routers to test (up to 5)")
+
+    parser.add_argument("-c", "--command", type=str, choices=["a", "i", "r", "c"],
+                        help="Enter the command type: a (author), i (init), r (routing table), c (crash)")
+
+    parser.add_argument("-t", "--target_router", type=str, choices=["1", "2", "3", "4", "5"],
+                        help="Enter the target router id (1 to 5")
+
+    args = parser.parse_args()
+
+    build_topology_file(args.router_count)
 
     subprocess.call("rm -R ./output", shell=True)
     subprocess.call("mkdir ./output", shell=True)
 
-    #subprocess.call("pkill assignment3", shell=True)
+    if args.command == "a":
+        print "got a"
+        #subprocess.call(CONTROLLER + " -t " + TOPOLOGY_FILE + OUTPUT_RULES + AUTHOR_COMMAND + " &", shell=True)
+    elif args.command == "i":
+        print "got i"
+        #subprocess.call(CONTROLLER + " -t " + TOPOLOGY_FILE + OUTPUT_RULES + INIT_COMMAND + " &", shell=True)
+    elif args.command == "r":
+        print "got r"
+        #subprocess.call(CONTROLLER + " -t " + TOPOLOGY_FILE + OUTPUT_RULES + ROUTING_TABLE_COMMAND + " &", shell=True)
+    elif args.command == "c":
+        print "got c"
+        #subprocess.call(CONTROLLER + " -t " + TOPOLOGY_FILE + OUTPUT_RULES + CRASH_COMMAND + " &", shell=True)
 
-    #subprocess.call("../assignment3 40000 1> " + APPLICATION_OUTPUT + " 2> " + APPLICATION_ERROR + " &", shell=True)
-    #time.sleep(1)
 
-    subprocess.call(CONTROLLER + " -t " + TOPOLOGY_FILE + OUTPUT_RULES + AUTHOR_COMMAND + " &", shell=True)
     #time.sleep(1)
     #subprocess.call(CONTROLLER + " -t " + TOPOLOGY_FILE + OUTPUT_RULES + INIT_COMMAND + " &", shell=True)
     #time.sleep(1)
